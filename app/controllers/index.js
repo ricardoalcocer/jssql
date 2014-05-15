e4bab18df42a762f75a923b29b47cc552e9796cd
Ricardo Alcocer <alco@ricardoalcocer.com>
2014-05-15 12:51:28 -0700

    first commit

---
+++ app/controllers/index.js
@@ -0,0 +1,68 @@
+/**
+* this is the main index.js file
+* @class Index
+*/
+
+/**
+* This is the doClick Method
+* @method doClick
+* 
+*/
+function doClick(e) {
+    alert($.label.text);
+}
+
+var DBH=require('com.alcoapps.dbhelper');
+var db=new DBH.dbhelper('/alco.sqlite','alco');
+	
+	// get records into variable
+	var myTable=db.get({
+		fields 	: '*',
+		table 	: 'events',
+		where 	: 'country like "U%"',
+		order 	: 'id DESC'
+	});
+	console.log(myTable);
+
+	// add record
+	var rowId=db.set({
+		table: 'events',
+		data : {
+			country 	: 'Puerto Rico',
+			name 		: 'TiConf PR'
+		}
+	});
+	console.log('Inserted: ' + rowId);
+
+	// get records with callback
+	db.get({
+		fields 	: '*',
+		table 	: 'events',
+		//where 	: 'country="US"',
+		//order 	: 'id DESC'
+	},function(evt){
+		console.log(evt);
+	});
+
+	// edit record
+	var rowsAffected=db.edit({
+		table 	: 'events',
+		data 	:{
+			name : 'xTiConf NY',
+			country : 'PR'
+		},
+		where 	: 'id = 1'
+	});
+
+	// delete record(s)
+	var rowsAffected=db.delete({
+		table 	: "events",
+		where 	: 'name="xTiConf PR"'
+	});
+
+	db.close();
+
+//var reporter=require('com.alcoapps.reportdeviceinfo');
+//console.log(reporter.report());
+
+$.index.open();
